2b446767df87b29fe55512c1c6b4f419a1e7c774
DesarrolloIuvade <dev@iuvade.com>
2024-07-15 06:52:42 -0500

    star

--- pages/starAlignment.vue
+++ pages/starAlignment.vue
@@ -1,37 +1,46 @@
 <template>
-    <div>
-      <h2 class="text-xl text-center font-black pb-6 pt-9 sm:pt-14">Star Alignment - Alineamiento Multiple</h2>
-      <div>
-        <label for="numSequences">Número de secuencias: </label>
-        <input class ="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-        type="number" v-model="numSequences" @change="generateSequenceInputs" min="1" />
-      </div>
-
-      <div v-if="numSequences > 0">
-        <div v-for="(index) in numSequences" :key="index">
-          <label :for="'seq' + index"> Secuencia {{ index }} :</label>
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          type="text" v-model="sequenceInputs[index - 1]" :id="'seq' + index" />
-        </div>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
-        @click="alignSequences">Alinear</button>
-      </div>
-
-      <div v-if="alignedSequences.length > 0">
-        <h3>Secuencias Alineadas</h3>
-        <table v-for="(alignment, index) in alignedSequences" :key="index" class="table-auto mb-4">
-          <tbody>
-            <tr>
-              <td v-for="(char, idx) in alignment" :key="'seq1-' + idx" :class="getColorByChar(char)"
-                class="cellAlignment">
-                {{ char }}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-
+  <div>
+    <h2 class="text-xl text-center font-black pb-6 pt-9 sm:pt-14">Star Alignment - Alineamiento Multiple</h2>
+    <div class="font-sans justify-center items-center p-6 flex">
+      <div class="w-full max-w-5xl">
+        <form @submit.prevent="smithWaterman" class="rounded px-8 pt-6 pb-8 mb-4">
+          <div>
+            <label for="numSequences" class="block text-gray-700 text-sm font-bold mb-2">Número de secuencias: </label>
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="number" v-model="numSequences" @change="generateSequenceInputs" min="1" />
+          </div>
+          <div v-if="numSequences > 0">
+            <div v-for="(index) in numSequences" :key="index">
+              <label :for="'seq' + index" class="block text-gray-700 text-sm font-bold mb-2"> Secuencia {{ index }}
+                :</label>
+              <input
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text" v-model="sequenceInputs[index - 1]" :id="'seq' + index" />
+            </div>
+            <button
+              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              @click="alignSequences">Alinear</button>
+          </div>
+        </form>
       </div>
     </div>
+
+
+    <div class="font-sans justify-center items-center p-6 flex flex-col">
+      <h5>Secuencias Alineadas</h5>
+      <table v-for="(alignment, index) in alignedSequences" :key="index" class="table-auto mb-4">
+        <tbody>
+          <tr>
+            <td v-for="(char, idx) in alignment" :key="'seq1-' + idx" :class="getColorByChar(char)"
+              class="cellAlignment">
+              {{ char }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
   
 <script>
@@ -113,14 +122,15 @@
         const alignedSequences = this.multipleSequenceAlignment(sequences, starIndex);
         this.alignedSequences = alignedSequences;
       },
+
       alignPair(seq1, seq2) {
         const m = seq1.length;
         const n = seq2.length;
         const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
-  
+
         for (let i = 0; i <= m; ++i) dp[i][0] = i * -2;
         for (let j = 0; j <= n; ++j) dp[0][j] = j * -2;
-  
+
         for (let i = 1; i <= m; ++i) {
           for (let j = 1; j <= n; ++j) {
             dp[i][j] = Math.max(
@@ -130,11 +140,11 @@
             );
           }
         }
-  
+
         let alignedSeq1 = '';
         let alignedSeq2 = '';
         let i = m, j = n;
-  
+
         while (i > 0 && j > 0) {
           if (i > 0 && j > 0 && dp[i][j] === dp[i - 1][j - 1] + this.score(seq1[i - 1], seq2[j - 1])) {
             alignedSeq1 = seq1[i - 1] + alignedSeq1;
@@ -151,8 +161,22 @@
             --j;
           }
         }
+
+        while (i > 0) {
+          console.log("i:" , i)
+          alignedSeq1 = seq1[i - 1] + alignedSeq1;
+          --i;
+        }
+
+        while (j > 0) {
+          console.log("j: " ,j)
+          alignedSeq1 = '-' + alignedSeq1;
+          alignedSeq2 = seq2[j - 1] + alignedSeq2;
+          --j;
+        }
         return [alignedSeq1, alignedSeq2];
       },
+
 
       balanceSequence(s1, s2) {
         let maxS, minS;
@@ -166,21 +190,29 @@
   
         let i = 0, j = 0;
         let ans = '';
-        while (j < maxS.length || i < minS.length) {
-          if (maxS[j] === minS[i] && maxS[j] !== '-') {
+        while (j < maxS.length) {
+          if (maxS[j] === minS[i]) {
             ans += maxS[j];
             i++;
             j++;
-          } else if (maxS[j] === '-') {
+          } else {
             ans += '-';
-            j++;
-          } else if (minS[i] === '-') {
-            ans += '-';
-            i++;
+            if(maxS[j] === '-'){
+              j++;
+            }
+            else {
+              i++;
+            }
           }
+        }
+
+        while(i < minS.length){ 
+          ans += minS[i];
+          i++;
         }
         return ans;
       },
+
       getAlignment(s1, s2, s3) {
         let ans = '';
         let i = 0;
@@ -193,6 +225,7 @@
         }
         return ans;
       },
+
       multipleSequenceAlignment(sequences, starIndex) {
         const n = sequences.length;
         const alignedSequences = [];
@@ -204,13 +237,26 @@
         }
   
         let balance = alignedSequences[0][0];
+        console.log("Alineciones guardadas: " , alignedSequences)
         for (let i = 1; i < n - 1; i++) {
+          if(balance === alignedSequences[i][0]) continue;
           balance = this.balanceSequence(balance, alignedSequences[i][0]);
         }
-  
+        console.log("Alineciones guardadas despues: " , alignedSequences)
+        console.log("STAR: " , balance)
         const result = [balance];
         for (let i = 0; i < n - 1; i++) {
-          const r = this.getAlignment(balance, alignedSequences[i][1], alignedSequences[i][0]);
+          let r = '';
+          let m = 0;
+          for(const element of balance){
+            if(element === alignedSequences[i][0][m]){
+              r += alignedSequences[i][1][m++];
+            }
+            else{
+              r += '-';
+            }
+            console.log("Alineado: " , r);
+          }
           result.push(r);
         }
         return result;
